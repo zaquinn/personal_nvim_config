9b2dcc792470961f2d20751196e3438c4e597fc9
zaquinn <ayzick120@gmail.com>
2025-02-03 14:57:16 -0300

    fix: fix treesitter plugin config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,35 +1,39 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		ensure_installed = {
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"zig",
-			"html",
-			"css",
-			"bash",
-      "markdown_inline",
-			"lua",
-			"dockerfile",
-			"gitignore",
-			"python",
-      "sql",
-      "go",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-	},
-  lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			indent = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"markdown",
+				"json",
+				"javascript",
+				"typescript",
+				"yaml",
+				"zig",
+				"html",
+				"css",
+				"bash",
+				"markdown_inline",
+				"lua",
+				"dockerfile",
+				"gitignore",
+				"python",
+				"sql",
+				"go",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = true,
+			},
+		})
+	end,
+	lazy = false,
 }
